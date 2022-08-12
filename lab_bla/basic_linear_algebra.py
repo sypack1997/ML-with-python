1b592c9fd88e58972fd1444b9d9b601572646384
sypack1997 <sypack1997@naver.com>
2022-08-12 18:11:28 +0900

    Upload_4

--- lab_bla/basic_linear_algebra.py
+++ lab_bla/basic_linear_algebra.py
@@ -24,38 +24,39 @@ def scalar_vector_product(alpha, vector_variable):
 
 
 def matrix_size_check(*matrix_variables):
-    return 
+    return all([len(set(len(matrix[0]) for matrix in matrix_variables)) == 1])and all ([len(matrix_variables[0]) == len(matrix) for matrix in matrix_variables]) # 첫번째 all은 행의 길이가 같을 경우, 두번째 all은 열의 길이가 같을 경우
 
 
 def is_matrix_equal(*matrix_variables):
-    return None
+    print([matrix for matrix in zip(*matrix_variables)])
+    return all([all([len(set(row)) == 1 for row in zip(*matrix)] for matrix in zip(*matrix_variables))])
 
 
 def matrix_addition(*matrix_variables):
     if matrix_size_check(*matrix_variables) == False:
         raise ArithmeticError
-    return None
+    return [[sum(row) for row in zip(*matrix)] for matrix in zip(*matrix_variables)]
 
 
 def matrix_subtraction(*matrix_variables):
     if matrix_size_check(*matrix_variables) == False:
         raise ArithmeticError
-    return None
+    return [[row[0]*2 - sum(row) for row in zip(*matrix)] for matrix in zip(*matrix_variables)]
 
 
 def matrix_transpose(matrix_variable):
-    return None
+    return [[element for element in row] for row in zip(*matrix_variable)]
 
 
 def scalar_matrix_product(alpha, matrix_variable):
-    return None
+    return [scalar_vector_product(alpha, row) for row in matrix_variable]
 
 
 def is_product_availability_matrix(matrix_a, matrix_b):
-    return None
+    return len([column_vector for column_vector in zip(*matrix_a)]) == len(matrix_b)
 
 
 def matrix_product(matrix_a, matrix_b):
     if is_product_availability_matrix(matrix_a, matrix_b) == False:
         raise ArithmeticError
-    return None
+    return [[sum(a*b for a,b in zip(row_a, column_b)) for column_b in zip(*matrix_b)] for row_a in matrix_a]
